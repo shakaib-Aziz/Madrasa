988e1112508a61279ad9da179aacefb7768e9392
S-H-A-B-B-Y <bsef20a507@pucit.edu.pk>
2023-06-20 07:02:39 -0700

    Done with layout problems

---
+++ app/src/main/java/com/example/madrasa/SaveDailyTasks.java
@@ -0,0 +1,13 @@
+package com.example.madrasa;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SaveDailyTasks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_save_dailytasks);
+    }
+}
